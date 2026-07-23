0060523ef764e9b8a10ff16855e8fc0a5421eca4
ashutoshtiwa2007 <ashutosh.tiwa2007@gmail.com>
2026-07-23 15:16:46 +0530

    task

--- eventloop/task1.js
+++ eventloop/task1.js
@@ -1,20 +1,27 @@
 const f1 = () => {
-    console.log("f1");
+    console.log("f1 starts");
+    f2();
+    console.log("f1 running");
+    console.log("f1 end");
 };
 
 const f2 = () => {
-    console.log("f2");
+    console.log("f2 starts");
+    f3();
+    console.log("f2 running");
+    console.log("f2 end");
 };
 
 const f3 = () => {
-    console.log("f3");
+    console.log("f3 starts");
+    console.log("f3 running");
+    console.log("f3 end");
 };
 
 function main() {
-    console.log("main");
+    console.log("main starts");
     f1();
-    f2();
-    f3();
+    console.log("main running");
     console.log("main end");
 }
 
