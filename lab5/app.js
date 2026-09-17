3c3f9f48ddcd1c592c548d7ed6741ad3886ed5de
ashutoshtiwa2007 <ashutosh.tiwa2007@gmail.com>
2026-09-17 13:58:52 +0530

    async routing and express js

--- lab5/app.js
+++ lab5/app.js
@@ -1,6 +1,43 @@
-import express from 'express'
-const app = express()
-app.get("/",(req,res)=>{
-    res.send("<h1>Hello Express</h1>")
-})
-app.listen(2000,()=>console.log("sever is gunning"))
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello Express</h1>");
+});
+
+app.get("/about", (req, res) => {
+    res.send("<h1>About Page</h1>");
+});
+
+app.post("/login", (req, res) => {
+    res.send({
+        msg: "User login"
+    });
+});
+
+app.put("/user/update/1", (req, res) => {
+    res.send({
+        msg: "Update user"
+    });
+});
+
+app.delete("/user/1", (req, res) => {
+    res.send({
+        msg: "Delete user"
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).send("Not found");
+});
+
+const server = app.listen(2000, () => {
+    console.log("Server is running on port 2000");
+});
+
+server.on("error", (err) => {
+    console.log("Server error:", err);
+});
---
+++ lab5/readme.md
@@ -0,0 +1,14 @@
+### Express 
+fast, unpolished , minimalist web framework for Node.js
+1,create npm project `npm init -y`
+2.update pakage.json
+`type:module`
+3.install nodemon with `npm i nodemon -D`
+4.install express with nom i express 
+5.update package.json
+
+```
+"scrips';[
+    "start ":"node app.js"
+    "dev":"nodemon app.js"
+]
